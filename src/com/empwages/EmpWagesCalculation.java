fcc251c970d1a7a8e5361ff2d2cbfd660fa5633b
soham874 <soham874@gmail.com>
2020-12-01 11:07:36 +0530

    Refactored : Removed welcome message and added attendance check

--- src/com/empwages/EmpWagesCalculation.java
+++ src/com/empwages/EmpWagesCalculation.java
@@ -3,6 +3,10 @@ package com.empwages;
 public class EmpWagesCalculation {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!!");
+		double check = Math.floor(Math.random() * 10 % 2);
+		if (check == 0)
+			System.out.println("Employee is absent.");
+		else
+			System.out.println("Employee is present.");
 	}
 }
