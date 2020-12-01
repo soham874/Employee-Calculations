c813937ddb8b3a7efb10554d803f2e654c381452
soham874 <soham874@gmail.com>
2020-12-01 14:45:36 +0530

    Added : Maximum workable days and hours

--- src/com/empwages/EmpWagesCalculation.java
+++ src/com/empwages/EmpWagesCalculation.java
@@ -7,9 +7,24 @@ public class EmpWagesCalculation {
 	public static void main(String[] args) {
 		randomise simulate=new randomise();
 		
-		for (int day=1; day <= 20; day ++) {
-			double wage=simulate.runsimulation();
-			System.out.println("Employee wage for day " + day + " is Rs." + wage);
+		int MAXIMUM_WORK_HOURS = 100;
+		int MAXIMUM_WORK_DAYS = 20;
+		
+		int workdays = 1;
+		int workhours = 0;
+		
+		while ( workdays <= MAXIMUM_WORK_DAYS && workhours <= MAXIMUM_WORK_HOURS ) {
+			
+			double currentworkhour = simulate.getworkhours();
+			double currentwage = simulate.getWage(currentworkhour);
+			
+			System.out.println("Employee worked for " + currentworkhour + " hours. The wage for day " + workdays + " is Rs."+ currentwage);
+			workhours+=currentworkhour;
+			workdays++;
 		}
+		
+		System.out.println("Total hours worked is " + workhours);
+		
+		
 	}
 }
--- src/com/random_generator/randomise.java
+++ src/com/random_generator/randomise.java
@@ -1,24 +1,32 @@
 package com.random_generator;
 
 public class randomise {
-	public double runsimulation( ) {
-		int WAGE_PER_HOUR = 20;
+	public double getworkhours( ) {
+		
 		int IS_FULL_DAY = 8;
 		int IS_HALF_DAY = 4;
-		double wage = 0;
-
+		
+		double empHours = 0;
 		double check = Math.floor(Math.random() * 10 % 3);
 		switch ((int) check) {
 		case 0:
-			wage = 0;
+			empHours = 0;
 			break;
 		case 1:
-			wage = WAGE_PER_HOUR * IS_HALF_DAY;
+			empHours = IS_HALF_DAY;
 			break;
 		case 2:
-			wage = WAGE_PER_HOUR * IS_FULL_DAY;
+			empHours = IS_FULL_DAY;
 			break;
 		}
+		return empHours;
+	}
+	
+	public double getWage(double empHours) {
+				
+		double wage = 0;
+		int WAGE_PER_HOUR = 20;
+		wage = empHours * WAGE_PER_HOUR;
 		return wage;
 	}
 }
