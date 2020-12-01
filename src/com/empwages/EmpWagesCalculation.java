083b9d9c386dd75fb12618ff89efda3bfaa844d1
soham874 <soham874@gmail.com>
2020-12-01 11:15:47 +0530

    Added : Daily wage calculations

--- src/com/empwages/EmpWagesCalculation.java
+++ src/com/empwages/EmpWagesCalculation.java
@@ -3,10 +3,17 @@ package com.empwages;
 public class EmpWagesCalculation {
 
 	public static void main(String[] args) {
+		int WAGE_PER_HOUR = 20;
+		int IS_FULL_DAY = 8;
+		double wage = 0;
+
 		double check = Math.floor(Math.random() * 10 % 2);
-		if (check == 0)
-			System.out.println("Employee is absent.");
-		else
-			System.out.println("Employee is present.");
+		if (check == 0) {
+			wage = WAGE_PER_HOUR * IS_FULL_DAY;
+
+		} else {
+			wage = 0;
+		}
+		System.out.println("Employee wage for day is Rs. " + wage);
 	}
 }
