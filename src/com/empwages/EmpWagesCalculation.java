ae3eeb3ee102f815236ebeaf17dd8671db6f13e4
soham874 <soham874@gmail.com>
2020-12-01 11:22:18 +0530

    Added : Half Day wage calculations

--- src/com/empwages/EmpWagesCalculation.java
+++ src/com/empwages/EmpWagesCalculation.java
@@ -5,15 +5,17 @@ public class EmpWagesCalculation {
 	public static void main(String[] args) {
 		int WAGE_PER_HOUR = 20;
 		int IS_FULL_DAY = 8;
+		int IS_HALF_DAY = 4;
 		double wage = 0;
 
-		double check = Math.floor(Math.random() * 10 % 2);
-		if (check == 0) {
+		double check = Math.floor(Math.random() * 10 % 3);
+		if (check == 2) 
 			wage = WAGE_PER_HOUR * IS_FULL_DAY;
-
-		} else {
-			wage = 0;
-		}
+		else if (check == 1) 
+			wage = WAGE_PER_HOUR * IS_HALF_DAY;
+		else
+			wage = 0;;
+			
 		System.out.println("Employee wage for day is Rs. " + wage);
 	}
 }
