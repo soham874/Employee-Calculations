af2e703136aca597ddd1be2d2c3702be2560aba9
soham874 <soham874@gmail.com>
2020-12-03 18:51:23 +0530

    Refactored : Storing total wages in an array list

--- src/com/empwages/EmpWagesCalculation.java
+++ src/com/empwages/EmpWagesCalculation.java
@@ -1,13 +1,13 @@
 package com.empwages;
 
 import com.random_generator.Randomise;
+import java.util.ArrayList;
 
 public class EmpWagesCalculation {
 
 	public static int MAXIMUM_WORK_HOURS = 0;
 	public static int MAXIMUM_WORK_DAYS = 0;
-	public static double[] companyWages = new double[100];
-	public static int position = 0;
+	public static ArrayList<Double> companyWages = new ArrayList<Double>();
 	public static String companyName = "";
 	
 	public static void calculate(String companyNameInput,int maximumWorkHours,int maximumWorkDays, int wagePerHour) {
@@ -38,7 +38,6 @@ public class EmpWagesCalculation {
 
 		System.out.println("Total hours worked is " + workhours);
 		System.out.println("Total earnings of the employee is Rs." + totalWage);
-		companyWages[position] = totalWage;
-		position++;
+		companyWages.add(totalWage);
 	}
 }
