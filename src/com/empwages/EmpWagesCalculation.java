780f506e53e141552a556d0cdd454af06f755c1e
soham874 <soham874@gmail.com>
2020-12-04 15:18:43 +0530

    Added : Storing company names and daily wages in list

--- src/com/empwages/EmpWagesCalculation.java
+++ src/com/empwages/EmpWagesCalculation.java
@@ -8,16 +8,18 @@ public class EmpWagesCalculation {
 	public static int MAXIMUM_WORK_HOURS = 0;
 	public static int MAXIMUM_WORK_DAYS = 0;
 	public static ArrayList<Double> companyWages = new ArrayList<Double>();
-	public static String companyName = "";
+	public static ArrayList<ArrayList<Double> > dailyWages = new ArrayList<ArrayList<Double>>();
+	public static ArrayList<String> companyName = new ArrayList<String>();
 	
 	public static void calculate(String companyNameInput,int maximumWorkHours,int maximumWorkDays, int wagePerHour) {
 		
+		dailyWages.add(new ArrayList<Double>());
 		MAXIMUM_WORK_HOURS = maximumWorkHours;
 		MAXIMUM_WORK_DAYS = maximumWorkDays;
-		companyName = companyNameInput;
+		companyName.add(companyNameInput);
 		Randomise.WAGE_PER_HOUR = wagePerHour;
 		
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~For company " + companyName + " ~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~For company " + companyName.get(companyName.size()-1) + " ~~~~~~~~~~~~~~~~~~~~~~");
 		Randomise simulate = new Randomise();
 
 		int workdays = 1;
@@ -31,11 +33,13 @@ public class EmpWagesCalculation {
 
 			System.out.println("Employee worked for " + currentworkhour + " hours. The wage for day " + workdays
 					+ " is Rs." + currentwage);
+			dailyWages.get(companyName.size()-1).add(workdays-1, currentwage);
 			totalWage += currentwage;
 			workhours += currentworkhour;
 			workdays++;
+			
 		}
-
+		//System.out.println(dailyWages);
 		System.out.println("Total hours worked is " + workhours);
 		System.out.println("Total earnings of the employee is Rs." + totalWage);
 		companyWages.add(totalWage);
